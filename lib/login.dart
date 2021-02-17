be5f916ae34a74ec75535b2b73ff43e88cfa91e5
Irina Hristova <hristova119@gmail.com>
2021-02-17 18:41:02 +0200

    Init login UI

---
+++ lib/login.dart
@@ -0,0 +1,11 @@
+library login;
+
+export './src/config/language.dart';
+export './src/widget/login_fresh.dart';
+export './src/service/type_login.dart';
+export './src/widget/footer_login.dart';
+export './src/widget/login_user_password.dart';
+export './src/widget/login_loading.dart';
+export './src/widget/login_reset_password.dart';
+export './src/widget/login_sign_up.dart';
+export './src/service/sign_up_model.dart';
---
+++ lib/src/service/type_login.dart
@@ -0,0 +1,13 @@
+class TypeLogo {
+  static String facebook = 'assets/images/facebook.png';
+  static String google = 'assets/images/google.png';
+  static String apple = 'assets/images/apple.png';
+  static String userPassword = 'assets/images/user_password.png';
+}
+
+class LoginTypeLoginModel {
+  Function callFunction;
+  String logo;
+
+  LoginTypeLoginModel({this.logo, this.callFunction});
+}
